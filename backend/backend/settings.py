598946fa5ae7627f8efddf3d5c797051fa81eeed
Anton K <executor1@gmail.com>
2025-04-02 01:16:21 +0300

    adding setup.cfg

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,11 +8,11 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['158.160.3.109', 
+ALLOWED_HOSTS = ['158.160.3.109',
                  'localhost',
                  '127.0.0.1',
                  'kochuev.zapto.org'
-                 ] 
+                 ]
 
 
 # Application definition
